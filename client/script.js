bf127ba4394cde0519ea206bc757a154d5ec0c77
ShilpiS27 <119177827+ShilpiS27@users.noreply.github.com>
2023-03-21 09:26:31 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('http://127.0.0.1:5000', {
+  const response = await fetch('https://programmingai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
